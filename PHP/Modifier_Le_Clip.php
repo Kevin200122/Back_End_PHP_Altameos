5a718d1c1a1bbb8a2ac4a55d17d044c61e60304f
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2023-10-18 15:18:10 +0200

    Modifier_ce_clip

--- PHP/Modifier_Le_Clip.php
+++ PHP/Modifier_Le_Clip.php
@@ -9,26 +9,27 @@ if ($_SESSION['role'] !== 'admin') {
     header('Location: Accueil2.php'); // Rediriger vers une page d'accueil appropriée pour les utilisateurs normaux
 }
 
-if(isset($_GET['id']) && trim($_GET['id']) != '') {
+if (isset($_GET['id']) && trim($_GET['id']) != '') {
     $getid = $_GET['id'];
     
     $req = $conn->prepare('SELECT * FROM clips WHERE id = ?');
-    $clip= $req->fetch();
     $req->execute(array($getid));
-    if($req->rowCount() == 1) {
+    
+    if ($req->rowCount() == 1) {
         $donnees = $req->fetch();
-        if(isset($_POST['valider'])) {
+        
+        if (isset($_POST['valider'])) {
             $emplacement = htmlspecialchars($_POST['emplacement']);
             $Auteur = htmlspecialchars($_POST['Auteur']);
             $Nom = htmlspecialchars($_POST['Nom']);
             $id_categorie = htmlspecialchars($_POST['id_categorie']);
             
-            if(!empty($emplacement) AND !empty($emplacement) AND !empty($Auteur) AND !empty($Nom) AND !empty($id_categorie)) {
-                $req = $conn->prepare('UPDATE clips SET Auteur = ?, Nom = ?, WHERE id = ?');
-                $req->execute(array(  $Auteur, $Nom, $getid));
-                header('Location: Modifier_Le_Clip');
+            if (!empty($emplacement) && !empty($Auteur) && !empty($Nom) && !is_numeric($id_categorie)) {
+                $req = $conn->prepare('UPDATE clips SET Auteur = ?, Nom = ?, emplacement = ?, id_categorie = ? WHERE id = ?');
+                $req->execute(array($Auteur, $Nom, $emplacement, $id_categorie, $getid));
+                header('Location: Modifier_Le_Clip.php'); // Corrigé : Ajout de l'extension du fichier
             } else {
-                echo "Veuillez remplir tous les champs";
+                echo "Veuillez remplir tous les champs correctement.";
             }
         }
     } else {
@@ -38,51 +39,52 @@ if(isset($_GET['id']) && trim($_GET['id']) != '') {
 
 $req = $conn->query('SELECT * FROM clips');
 $clips = $req->fetchAll();
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Modifier le clips</title>
+<title>Modifier le clip</title>
 </head>
+
 <?php include('../include/header.php'); ?>
-<?php if(isset($_GET['id']) and !empty($_GET['id'])): ?>
+
+<?php if (isset($_GET['id']) && !empty($_GET['id'])): ?>
     <div class="container mt-4">
     <h2><?= htmlspecialchars($donnees['Auteur']); ?></h2>
     <p><?= nl2br(htmlspecialchars($donnees['emplacement'])); ?></p>
-    <p>Auteur: <strong><?= nl2br(htmlspecialchars($donnees['Auteur'])); ?></p></strong>
-    <p>Contenu: <strong><?= nl2br(htmlspecialchars($donnees['Nom'])); ?></p></strong>
-    <p>Catégorie: <strong><?= nl2br(htmlspecialchars($donnees['id_categorie'])); ?></p></strong>
+    <p>Auteur: <strong><?= nl2br(htmlspecialchars($donnees['Auteur'])); ?></strong></p>
+    <p>Contenu: <strong><?= nl2br(htmlspecialchars($donnees['Nom'])); ?></strong></p>
+    <p>Catégorie: <strong><?= nl2br(htmlspecialchars($donnees['id_categorie'])); ?></strong></p>
     <a href="?id=<?= $donnees['id']; ?>" class="btn btn-primary">Modifier le clip</a>
     </div>
     <hr>
     
-    
-    
     <div class="container mt-4">
     <form method="POST" action="">
     <div class="form-group">
-    <label for="title">Titre</label>
-    <input type="text" class="form-control" id="Titre" name="titre" placeholder="Titre" value=""<?php echo $donnees['Auteur']; ?>"><!-- title est une colonne de la bdd elle est initie par $titre qui vaut title $titre = htmlspecialchars($_POST['title']); on faisons cela dans notre placeholder on aura les donnees du clip sélectionne  -->
-    </div>
-    <div class="form-group">
-    <label for="auteur">Auteur</label>
-    <input type="text" class="form-control" id="Auteur" name="nom" placeholder="nom" value="<?php echo $donnees['Auteur']; ?>">
+    <label for="Auteur">Auteur</label>
+    <input type="text" class="form-control" id="Auteur" name="Auteur" placeholder="Auteur" value="<?= $donnees['Auteur']; ?>">
     </div>
     <div class="form-group">
     <label for="Nom">Nom</label>
-    <input type="text" class="form-control" id="Nom" name="nom" placeholder="nom" value="<?php echo $donnees['Nom']; ?>">
+    <input type="text" class="form-control" id="Nom" name="Nom" placeholder="Nom" value="<?= $donnees['Nom']; ?>">
     </div>
     <div class="form-group">
-    <label for="emplacement">emplacement</label>
-    <input type="text" class="form-control" id="emplacement" name="emplacement" placeholder="emplacement" value="<?php echo $donnees['emplacement']; ?>">
+    <label for="emplacement">Emplacement</label>
+    <input type="text" class="form-control" id="emplacement" name="emplacement" placeholder="Emplacement" value="<?= $donnees['emplacement']; ?>">
+    </div>
+    <div class="form-group">
+    <label for="id_categorie">Catégorie</label>
+    <input type="text" class="form-control" id="id_categorie" name="id_categorie" placeholder="ID de catégorie" value="<?= $donnees['id_categorie']; ?>">
     </div>
     <button type="submit" class="btn btn-primary" name="valider">Valider</button>
     </form>
     </div>
     <?php endif; ?>
-    <?php include('../include/footer.php');?>
+    
+    <?php include('../include/footer.php'); ?>
+    
     
